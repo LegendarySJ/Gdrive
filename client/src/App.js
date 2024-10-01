47e593dbfe506f2f52814f4b9e4990c254b91b4e
LegendarySJ <srj63441@gmail.com>
2024-10-02 02:21:39 +0530

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import Upload from "./artifacts/contracts/Upload.sol/upload.json";
+import {useState, useEffect} from "react";
+import {ethers} from "ethers";
+import FileUpload from "./components/FileUpload";
+import Display from "./components/Display";
+import Modal from "./components/Modal";
 import './App.css';
 
 function App() {
+  const [account,setAccount] = useState("");
+  const [contract,setContract] = useState(null);
+  const [provider,setProvider] = useState(null);
+  const [modalOpen,setModalOpen] = useState(null);
+
+  useEffect(() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const loadProvider = async() =>{
+      if(provider){
+        await provider.send("eth_requestAccounts",[]);
+        const signer = provider.getSigner();
+        
+      }
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,7 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Upload = await hre.ethers.getContractFactory("Upload");
+  const Upload = await hre.ethers.getContractFactory("upload");
   const upload = await Upload.deploy();
 
   await upload.deployed();
